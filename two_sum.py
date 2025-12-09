36a7a89746d2340b81a5b4bf5000fda0816692e7
kenshi <k.minamiya@4nonome.com>
2025-12-09 15:18:17 +0900

    Add two_sum solution

---
+++ two_sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            sub = target - nums[i]
+            for j in range(i+1,len(nums)):
+                if nums[j] == sub:
+                    return [i,j]
