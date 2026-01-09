1b217dd71f49bce4dfb81c248726c9ed9e8ef0a6
kenshi <k.minamiya@4nonome.com>
2026-01-09 22:47:11 +0900

    add solutions

--- easy/228_SummaryRages.py
+++ easy/228_SummaryRages.py
@@ -1,26 +1,24 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        res = []
+        result = []
         if not nums:
-            return res
+            return result
 
         start = nums[0]
 
         for i in range(1, len(nums)):
-            # 連続していない場合
             if nums[i] != nums[i - 1] + 1:
                 end = nums[i - 1]
                 if start == end:
-                    res.append(str(start))
+                    result.append(str(start))
                 else:
-                    res.append(f"{start}->{end}")
+                    result.append(f"{start}->{end}")
                 start = nums[i]
 
-        # 最後の区間を追加
         end = nums[-1]
         if start == end:
-            res.append(str(start))
+            result.append(str(start))
         else:
-            res.append(f"{start}->{end}")
+            result.append(f"{start}->{end}")
 
-        return res
+        return result
