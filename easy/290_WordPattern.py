c9bdd397d97fd3edb346973ddc8a22d8d71f6613
kenshi <k.minamiya@4nonome.com>
2026-01-13 14:54:42 +0900

    add solutions

---
+++ easy/290_WordPattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        p2w = {}
+        w2p = {}
+        for p, w in zip(pattern, words):
+            if p in p2w and p2w[p] != w:
+                return False
+            if w in w2p and w2p[w] != p:
+                return False
+
+            p2w[p] = w
+            w2p[w] = p
+
+        return True
